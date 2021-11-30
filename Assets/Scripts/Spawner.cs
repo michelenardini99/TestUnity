c9e87b12c421a250132e4c45f25d975b88d00e23
Michele Nardini <michele.nardini3g@gmail.com>
2021-12-01 00:44:28 +0100

    Add files via upload

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cubePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Sono spawnato " + gameObject.name, gameObject);
+        Instantiate(cubePrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 0 = left click
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(cubePrefab, new Vector3(Random.Range(-5, 5),Random.Range(-5,5),1),cubePrefab.transform.rotation);
+        }
+    }
+}
